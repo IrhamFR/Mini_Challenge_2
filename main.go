c16b91f4eab3d929798523b75f8ed27c8ab48afd
IrhamFR <irham.f.rufdo@gmail.com>
2022-09-16 00:26:45 +0700

    update

--- main.go
+++ main.go
@@ -8,17 +8,26 @@ func main() {
 
 	var bill int
 	var discount int
+	var max int
 	var afterDiscount int
 
 	fmt.Println("voucher DumbMerchBerkah")
 	fmt.Println("Total Belanja:")
 	fmt.Scanf("%d", &bill)
 
-	fmt.Println("Diskon: 25%")
 	fmt.Scanf("%d", &discount)
 	if bill >= 50000 {
 		discount = 25
 	}
+	fmt.Println("Diskon:", discount, "%")
+
+	max = bill - (bill - discount)
+	if bill >= 50000 {
+		max = 20000
+	} else if bill <= 50000 {
+		max = 0
+	}
+	fmt.Println("Potongan:", max)
 
 	afterDiscount = bill - (bill * discount / 100)
 	fmt.Println("Total Pembayaran: ", afterDiscount)
@@ -29,11 +38,19 @@ func main() {
 	fmt.Println("Total Belanja:")
 	fmt.Scanf("%d", &bill)
 
-	fmt.Println("Diskon: 50%")
 	fmt.Scanf("%d", &discount)
 	if bill >= 70000 {
 		discount = 50
 	}
+	fmt.Println("Diskon:", discount, "%")
+
+	max = bill - (bill - discount)
+	if bill >= 70000 {
+		max = 45000
+	} else if bill <= 70000 {
+		max = 0
+	}
+	fmt.Println("Potongan:", max)
 
 	afterDiscount = bill - (bill * discount / 100)
 	fmt.Println("Total Pembayaran: ", afterDiscount)
