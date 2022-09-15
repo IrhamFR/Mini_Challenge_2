f712499a6bead787d9cef4671d8fac69d4323f4b
IrhamFR <irham.f.rufdo@gmail.com>
2022-09-15 20:54:41 +0700

    update

--- main.go
+++ main.go
@@ -6,35 +6,35 @@ import (
 
 func main() {
 
-  var bill int
-  var discount int 
-  var afterDiscount int
+	var bill int
+	var discount int
+	var afterDiscount int
 
-  fmt.Println("voucher DumbMerchBerkah")
-  fmt.Println("Total Belanja:")
-  fmt.Scanf("%d", &bill)
+	fmt.Println("voucher DumbMerchBerkah")
+	fmt.Println("Total Belanja:")
+	fmt.Scanf("%d", &bill)
 
-  fmt.Println("Diskon: 25%!s") 
-  fmt.Scanf("%d", &discount)
-  if bill >= 50000 {
-	discount = 25
-  }
+	fmt.Println("Diskon: 25%")
+	fmt.Scanf("%d", &discount)
+	if bill >= 50000 {
+		discount = 25
+	}
 
-  afterDiscount = bill - (bill * discount / 100)
-  fmt.Println("Total Pembayaran: " , afterDiscount)
+	afterDiscount = bill - (bill * discount / 100)
+	fmt.Println("Total Pembayaran: ", afterDiscount)
 
-  fmt.Println("=======================================================")
+	fmt.Println("=======================================================")
 
-  fmt.Println("voucher DumbMerchMurahBanget")
-  fmt.Println("Total Belanja:")
-  fmt.Scanf("%d", &bill)
+	fmt.Println("voucher DumbMerchMurahBanget")
+	fmt.Println("Total Belanja:")
+	fmt.Scanf("%d", &bill)
 
-  fmt.Println("Diskon: 50%")
-  fmt.Scanf("%d", &discount)
-  if bill >= 70000 {
-	discount = 50
-  }
-  
-  afterDiscount = bill - (bill * discount / 100)
-  fmt.Println("Total Pembayaran: " , afterDiscount)
-  }
+	fmt.Println("Diskon: 50%")
+	fmt.Scanf("%d", &discount)
+	if bill >= 70000 {
+		discount = 50
+	}
+
+	afterDiscount = bill - (bill * discount / 100)
+	fmt.Println("Total Pembayaran: ", afterDiscount)
+}
